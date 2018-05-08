8474e4b8ad3c2cbcff24d2f65785edf47a55473e
dusanradivojevic <r.dusan97@gmail.com>
2018-05-08 23:59:09 +0200

    Izmene u interfejsu PicerijaInterfejs.

--- Picerija2018/src/main/Picerija.java
+++ Picerija2018/src/main/Picerija.java
@@ -1,11 +1,17 @@
 package main;
 
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
 import main.interfejs.PicerijaInterfejs;
+import main.porudzbina.StavkaPorudzbine;
 
 public class Picerija implements PicerijaInterfejs{
+	
+	LinkedList<StavkaPorudzbine> porudzbina;	
 
 	@Override
-	public void serijalizuj() {
+	public void serijalizuj(LinkedList<StavkaPorudzbine> porudzbina, GregorianCalendar datum) {
 		// TODO Auto-generated method stub
 		
 	}
--- Picerija2018/src/main/interfejs/PicerijaInterfejs.java
+++ Picerija2018/src/main/interfejs/PicerijaInterfejs.java
@@ -1,8 +1,13 @@
 package main.interfejs;
 
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import main.porudzbina.StavkaPorudzbine;
+
 public interface PicerijaInterfejs {
 	
-	public void serijalizuj();
+	public void serijalizuj(LinkedList<StavkaPorudzbine> porudzbina, GregorianCalendar datum);
 	
 	public void dodajPorudzbinu(String naziv, double cena);
 }
